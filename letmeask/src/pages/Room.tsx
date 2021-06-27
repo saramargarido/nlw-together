405672a98d5b68d9fd72c324f9713986232902e2
saramargarido <saramargarido@gmail.com>
2021-06-26 23:19:46 -0300

    aula 4 - hook useRoom

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg'
@@ -6,30 +6,10 @@ import { Button } from '../components/Button'
 import { Question } from '../components/Question'
 import { RoomCode } from '../components/RoomCode'
 import { useAuth } from '../hooks/useAuth'
+import { useRoom } from '../hooks/useRoom'
 import { database } from '../services/firebase'
 
 import '../styles/room.scss'
-
-type FirebaseQuestions = Record<string, {
-    author: {
-        name: string;
-        avatar: string;
-    }
-    content : string;
-    isAnswered: boolean;
-    isHighlighted: boolean;
-}>
-
-type QuestionType = {
-    id: string,
-    author: {
-        name: string;
-        avatar: string;
-    }
-    content : string;
-    isAnswered: boolean;
-    isHighlighted: boolean;
-}
 
 type RoomParams = {
     id: string
@@ -38,33 +18,10 @@ type RoomParams = {
 export function Room() {
     const { user } = useAuth()
     const params = useParams<RoomParams>()
-    const roomId = params.id
     const [newQuestion, setNewQuestion] = useState('')
-    const [questions, setQuestions] = useState<QuestionType[]>([])
-    const [title, setTitle] = useState('')
+    const roomId = params.id
 
-    useEffect(() => {
-        const roomRef = database.ref(`rooms/${roomId}`)
-
-        //TODO: Em salas com muitas perguntas esse script (on) fica lento, ver -> eventos firebase, ex. Child Added, Changed, Removed... etc. Ao invés de ouvir o 'value'
-        roomRef.on('value', room => {
-            const databaseRoom = room.val()
-            const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {}
-
-            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
-                return {
-                    id: key,
-                    content: value.content,
-                    author: value.author,
-                    isHighlighted: value.isHighlighted,
-                    isAnswered: value.isAnswered
-                }
-            })
-
-            setTitle(databaseRoom.title)
-            setQuestions(parsedQuestions)
-        })
-    }, [roomId])
+    const { title, questions } = useRoom(roomId)
 
     async function handleSendQuestion(event: FormEvent) {
         event.preventDefault()
